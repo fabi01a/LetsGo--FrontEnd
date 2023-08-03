a2e818c87e4edd087662ef709caa0a0e08f0e6e8
Fabi01a <gonzalezfabs@gmail.com>
2023-08-03 15:02:29 -0700

    uncommented search to test if route works

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -6,61 +6,58 @@ import axios from 'axios';
 
 
 const Search = () => {
-//     const[address, setAddress] = useState("");
-//     const[maxDistance, setMaxDistance] = useState("");
+    const[address, setAddress] = useState("");
+    const[maxDistance, setMaxDistance] = useState("");
 
-//     const handleAddressChange = (e) => {
-//         setAddress(e.target.value);
-//     }
+    const handleAddressChange = (e) => {
+        setAddress(e.target.value);
+    }
 
-//     const handleMaxDistanceChange = (e) => {
-//         const userInput = e.target.value;
-//         if (!isNaN(userInput) && userInput !== '') {
-//             setMaxDistance(userInput);
-//         }
-//     }
+    const handleMaxDistanceChange = (e) => {
+        const userInput = e.target.value;
+        if (!isNaN(userInput) && userInput !== '') {
+            setMaxDistance(userInput);
+        }
+    }
 
-//     const handleSubmit = async () => {
-//         try {
-//             const response = await axios.get('https://letsgo-be-userprofile.onrender.com/get_campsite_data', {
-//                 params: {                   
-//                     address: address,
-//                     radius: maxDistance,  
-//                 },
+    const handleSubmit = async () => {
+        try {
+            const response = await axios.get('https://letsgo-be-userprofile.onrender.com/get_campsite_data', {
+                params: {                   
+                    address: address,
+                    radius: maxDistance,  
+                },
                 
-//             });
-//             console.log(response.data)
-//             //processed data received from backend/use this later to pass along to other page
-//             //const processedData = response.data;
+            });
+            console.log(response.data)
+            //processed data received from backend/use this later to pass along to other page
+            //const processedData = response.data;
             
-//         }   catch (error) {
-//             console.error('Error occured', error);
-//         }
-//     };
+        }   catch (error) {
+            console.error('Error occured', error);
+        }
+    };
 
     return (
-//         <div id="search-container">
-//             <div id='search-container--top'>
-//                 <img id="search-container--top--image"src="https://res.cloudinary.com/dvbdefnwx/image/upload/v1666621595/sample.jpg" alt="campsite tent"/> 
-//                 <div>Choose Your Destination</div>
-//             </div>
-//             <div id='search-container--middle'>
-//                 <div>
-//                     <input type="text" value={address} onChange={handleAddressChange} />
-//                     <div className="center-text bold-text">Starting Point</div>
-//                 </div>
-//                 <div>
-//                     <input type="text" value={maxDistance} onChange={handleMaxDistanceChange} />
-//                     <div className="center-text bold-text">Max Distance</div>
-//                 </div>
-//             </div>
-//             <div id='search-container--bottom'>
-//                 <SubmitButton handleSubmit={handleSubmit} />
-//             </div>
-//         </div>
-        <div>
-        Lets give it a shot! 
-        </div>   
+        <div id="search-container">
+            <div id='search-container--top'>
+                <img id="search-container--top--image"src="https://res.cloudinary.com/dvbdefnwx/image/upload/v1666621595/sample.jpg" alt="campsite tent"/> 
+                <div>Choose Your Destination</div>
+            </div>
+            <div id='search-container--middle'>
+                <div>
+                    <input type="text" value={address} onChange={handleAddressChange} />
+                    <div className="center-text bold-text">Starting Point</div>
+                </div>
+                <div>
+                    <input type="text" value={maxDistance} onChange={handleMaxDistanceChange} />
+                    <div className="center-text bold-text">Max Distance</div>
+                </div>
+            </div>
+            <div id='search-container--bottom'>
+                <SubmitButton handleSubmit={handleSubmit} />
+            </div>
+        </div>
     );
 };
 
