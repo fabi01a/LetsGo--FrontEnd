cacce22f4fccfa86bf2d1195293dfe555e0d3816
Fabi01a <gonzalezfabs@gmail.com>
2023-08-03 12:47:30 -0700

    commented out DisplayCampsites to troubleshoot why Search isnt working

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           {/* <Route path='/' element={<Home />} /> */}
           <Route path='/search' element={<Search />} />
-          <Route path='/search/campsites' element={<DisplayCampsites />} />
+          {/* <Route path='/search/campsites' element={<DisplayCampsites />} /> */}
      
         </Routes>
         
