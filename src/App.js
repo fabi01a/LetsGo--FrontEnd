01c425e5f324d567c10c6be5b6645a2095426619
Fabi01a <gonzalezfabs@gmail.com>
2023-08-03 14:55:53 -0700

    updated route default

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <Routes>
           {/* <Route path='/' element={<Home />} /> */}
-          <Route path='/search' element={<Search />} />
+          <Route index path='/' element={<Search />} />
           {/* <Route path='/search/campsites' element={<DisplayCampsites />} /> */}
      
         </Routes>
