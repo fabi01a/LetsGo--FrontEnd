de67d9aa1c3b1b5b2a362edbddf32c9862bb0bee
Fabi01a <gonzalezfabs@gmail.com>
2023-08-07 21:21:08 -0700

    CampsiteDetails response obj HTML  successfully striped

--- src/pages/CampsiteDetails.js
+++ src/pages/CampsiteDetails.js
@@ -3,24 +3,28 @@ import { useLocation } from 'react-router-dom';
 
 const CampsiteDetails = () => {
     const location = useLocation();
-    const campsiteData = location.state || [];
-    //check if state obj is null or not
-    console.log('State:', campsiteData);
+    const campsiteData = location.state || {};
+
+    //function to strip HTML tags from a string
+    const stripHtmlTags = (html) => {
+        const doc = new DOMParser().parseFromString(html, 'text/html');
+        return doc.body.textContent || '';
+    };
 
     return (
         <div>
-            <header>
+            {/* <header>
                 {campsiteData.facility_map_url ? (
                     <img src={campsiteData.facility_map_url} alt={`Map for ${campsiteData.name}`} />
                 ) : (
                     <h2>{campsiteData.name || 'N/A'}</h2>
                 )}
-            </header>
+            </header>  */}
             
             {/* <p>Address: {address}</p> */}
             <p>Phone: {campsiteData.facility_phone || 'N/A'}</p>
-            <p>Directions: {campsiteData.facility_directions || 'N/A'}</p>
-            <p>Description: {campsiteData.facility_description || 'N/A'}</p>
+            <p>Directions: {stripHtmlTags(campsiteData.facility_directions) || 'N/A'}</p>
+            <p>Description: {stripHtmlTags(campsiteData.facility_description)|| 'N/A'}</p>
             <p>Map: {campsiteData.facility_map_url || 'N/A'}</p>
         </div>
     );
