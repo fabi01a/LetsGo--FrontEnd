663f5f8e0f6c4454c4ffb55c39abaefe144a81fc
Fabi01a <gonzalezfabs@gmail.com>
2023-08-13 14:04:02 -0700

    reset to render url to troubleshoot CORS issue

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import authSlice from "../store/slices/auth";
 import useSWR from 'swr';
-import { fetcher } from "../utils/axios";
+import fetcher  from "../utils/axios";
 
 const UserProfile = () => {
     const dispatch = useDispatch();
@@ -18,25 +18,36 @@ const UserProfile = () => {
         dispatch(authSlice.actions.logout());
         navigate("/login");
     };
+    
     return (
-        <div className="w-full h-screen">
-            <div className="w-full p-6">
+        <div className="w-full h-screen p-6">
+            <h1>User Profile</h1>
+
+            <div className="mb-4">
+                {user.data ? (
+                    <div>
+                        <p>Email: {user.data.email}</p>
+                        <p>Other Profile Data: {user.data.otherField}</p>
+                    </div>
+                ) : (
+                    <p>Loading user profile...</p>
+                )}
+            </div>
+
+            <div className="text-center">
+                {user.data ? (
+                    <p>Welcome, {user.data?.username}</p>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
+
             <button
                 onClick={handleLogout}
                 className="rounded p-2 w-32 bg-red-700 text-black"
             >
                 Logout
             </button>
-        </div>
-        {
-            user.data ? (
-                <div className="w-full h-full text-center items-center">
-                    <p className="self-center my-auto">Welcome, {user.data?.username}</p>
-                </div>
-            ) : (
-                <p className="text-center items-center">Loading...</p>
-            )
-        }
         </div>
     );
 };
